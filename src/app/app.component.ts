e19c77d876feba1ae79319241da4fcfe308a626a
Miljan <miljan.grcic@gmail.com>
2020-11-19 21:39:23 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'posts-app';
+  user : User = null;
+  showTable = true;
+
+  selectUser(user: User) {
+    this.user = user;
+    this.showTable = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { PostComponent } from './components/post/post.component';
+import { CommentListComponent } from './components/comment-list/comment-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { UsersService } from './services/users.service';
+import { PostsService } from './services/posts.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserDetailsComponent,
+    PostListComponent,
+    PostComponent,
+    CommentListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [UsersService, PostsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
